48f397a6d6b9d2fd6688ed064578bf5e70ea1d2e
milkwatcher <146588862+MilkWatcher@users.noreply.github.com>
2025-11-05 19:14:29 +0000

    Implement find_max function to find maximum value

---
+++ lab3/lab33.cpp
@@ -0,0 +1,12 @@
+int find_max(int *arr, int size){
+int *ptr = arr;
+int max = *ptr;
+
+  for(int i = 0; i < size; i++){
+    ptr**;
+    if (*ptr > max){
+        max = *ptr;
+    }
+  }
+  return max;
+}
