34e47974c7dd291d4d198143e7f137f9438bb8b4
milkwatcher <146588862+MilkWatcher@users.noreply.github.com>
2025-11-05 19:17:02 +0000

    Add function to concatenate two strings

---
+++ lab3/lab38.cpp
@@ -0,0 +1,11 @@
+void concat_strings(char *dest, char *src) {
+    while (*dest != '\0') {
+        dest++;
+    }
+    while (*src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+}
